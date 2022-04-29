e8096faff38da53bf16bec3e57f1b067d160aa83
mdjfs <marcos.fuenmayorhtc@gmail.com>
2022-04-28 22:26:40 -0300

    merge

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-const custom = require("../node_modules/react-scripts/config/webpack.config");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -10,34 +7,4 @@ module.exports = {
     "@storybook/preset-create-react-app",
   ],
   framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
-  },
 };
-
-//stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-/*addons: [
-  "@storybook/addon-links",
-  "@storybook/addon-essentials",
-  "@storybook/addon-interactions",
-  "@storybook/preset-create-react-app",
-],
-framework: "@storybook/react",
-core: {
-  builder: "webpack5",
-},*/
-=======
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react"
-}
->>>>>>> 0f2deda (fixes)
