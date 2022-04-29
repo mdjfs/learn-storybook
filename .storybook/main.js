5b9caf992ab5df4fe08883ebb04fde1cc5a94f29
mdjfs <marcos.fuenmayorhtc@gmail.com>
2022-04-28 22:04:51 -0300

    test

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const custom = require("../node_modules/react-scripts/config/webpack.config");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -5,7 +7,21 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
 };
+
+//stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+/*addons: [
+  "@storybook/addon-links",
+  "@storybook/addon-essentials",
+  "@storybook/addon-interactions",
+  "@storybook/preset-create-react-app",
+],
+framework: "@storybook/react",
+core: {
+  builder: "webpack5",
+},*/
