dc428ab2d6f9501d0b9f69fdaca819e9cb0db9ac
mdjfs <marcos.fuenmayorhtc@gmail.com>
2022-04-28 22:22:18 -0300

    merge

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const custom = require("../node_modules/react-scripts/config/webpack.config");
 
 module.exports = {
@@ -25,3 +26,18 @@ framework: "@storybook/react",
 core: {
   builder: "webpack5",
 },*/
+=======
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": "@storybook/react"
+}
+>>>>>>> 0f2deda (fixes)
